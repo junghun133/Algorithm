87c5dd2672c92a53e3b20a075715dde3aa3abfdf
Park JungHoon <junghun5947@gmail.com>
2020-08-24 18:18:51 +0900

    42584 문제 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
    제한사항
    prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
    prices의 길이는 2 이상 100,000 이하입니다.
    입출력 예
    prices	return
    [1, 2, 3, 2, 3]	[4, 3, 1, 1, 0]

---
+++ src/com/pjh/example/algorithm/website/Programmers42584.java
@@ -0,0 +1,17 @@
+package com.pjh.example.algorithm.website;
+
+import java.util.*;
+
+public class Programmers42584 {
+    //https://programmers.co.kr/learn/courses/30/lessons/42584
+    static class Solution{
+        public static void main(final String[] args){
+            System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 2, 3})));
+            // return [4, 3, 1, 1, 0]
+        }
+
+        public static int[] solution(int[] prices) {
+            return prices;
+        }
+    }
+}
