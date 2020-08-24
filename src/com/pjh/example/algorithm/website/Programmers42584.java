1090f748465b58a885af62e480d63db67eea42d6
junghun5947 <junghun5947@gmail.com>
2020-08-24 22:39:37 +0900

    주식가격 이중 for문으로 해결

--- src/com/pjh/example/algorithm/website/Programmers42584.java
+++ src/com/pjh/example/algorithm/website/Programmers42584.java
@@ -4,14 +4,24 @@ import java.util.*;
 
 public class Programmers42584 {
     //https://programmers.co.kr/learn/courses/30/lessons/42584
-    static class Solution{
-        public static void main(final String[] args){
+    static class Solution {
+        public static void main(final String[] args) {
             System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 2, 3})));
             // return [4, 3, 1, 1, 0]
         }
 
         public static int[] solution(int[] prices) {
-            return prices;
+            int len = prices.length;
+            int[] answer = new int[len];
+            int i, j;
+            for (i = 0; i < len; i++) {
+                for (j = i + 1; j < len; j++) {
+                    answer[i]++;
+                    if (prices[i] > prices[j])
+                        break;
+                }
+            }
+            return answer;
         }
     }
 }
