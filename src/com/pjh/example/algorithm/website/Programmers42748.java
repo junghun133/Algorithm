8ce2debcfa65bcbf73428b64d2b880082e9f90c6
Park JungHoon <junghun5947@gmail.com>
2020-08-26 16:09:56 +0900

    정렬문제

--- src/com/pjh/example/algorithm/website/Programmers42748.java
+++ src/com/pjh/example/algorithm/website/Programmers42748.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Programmers42748 {
-
+    //https://programmers.co.kr/learn/courses/30/lessons/42748
     public static void main(final String[] args) {
         int[][] com = new int[][]{{2,5,3}, {4,4,1}, {1,7,3}};
         int[] arr = new int[]{1,5,2,6,3,7,4};
