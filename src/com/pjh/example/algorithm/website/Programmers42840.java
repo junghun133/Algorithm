ce5b19155174e5ee1e6dcbce9f8a961321a99bf0
Park JungHoon <junghun5947@gmail.com>
2020-08-26 18:09:17 +0900

    프로그래머스 42840 문제 세팅

---
+++ src/com/pjh/example/algorithm/website/Programmers42840.java
@@ -0,0 +1,15 @@
+package com.pjh.example.algorithm.website;
+
+import java.util.Arrays;
+
+public class Programmers42840 {
+    public static void main(final String[] args) {
+        int[] answer = new int[]{1,2,3,4,5};
+
+        System.out.println(Arrays.toString(solution(answer)));
+    }
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
