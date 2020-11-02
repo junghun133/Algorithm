1aef9ed2684acdd611e3d953ad5dd2ca2701cb45
junghun5947 <junghun5947@gmail.com>
2020-11-03 03:37:02 +0900

    팀 배정 알고리즘

---
+++ src/com/pjh/example/algorithm/website/Programmers42883_greedy.java
@@ -0,0 +1,22 @@
+package com.pjh.example.algorithm.website;
+
+public class Programmers42883_greedy {
+
+    public static void main(final String[] args) {
+        System.out.println(solution("1924", 2));
+    }
+
+    private static String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder(number);
+        for (int i = 0; i+1 < sb.length() && k>0; i++) {
+            if(sb.charAt(i) < sb.charAt(i+1)) {
+                sb.deleteCharAt(i);
+                i=-1;
+                k--;
+            }
+        }
+        if(k!=0)
+            sb.delete(sb.length()-k, sb.length());
+        return sb.toString();
+    }
+}
